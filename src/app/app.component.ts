bb2f122f8270729ab30cbcddb313b5db1472adcf
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-10 09:59:53 +0200

    Suppression du html css Angular Affichage d'un titre qui change au click d'un bouton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'promo-mai-2022-ng-pokedex-temp';
+  title = 'coucou';
+
+  onClick() {
+    this.title = "toto";
+  }
 }
