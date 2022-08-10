b0c0bf5cecdd2c7375fe4a34c329f4ec04f6afe0
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-10 10:41:06 +0200

    Ajout de ng-bootstrap avec "ng add @ng-bootstrap/ng-bootstrap"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PokemonListComponent } from './components/pokemon-list/pokemon-list.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
