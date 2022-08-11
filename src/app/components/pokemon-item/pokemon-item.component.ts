545e235a0e7f8b70b8b13d19d47206b4575ce1d7
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-11 11:51:03 +0200

    Passage du nom du pokemon au pokemon-item en utilisant le binding angluar @Input

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-item',
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-item.component.scss']
 })
 export class PokemonItemComponent implements OnInit {
+  @Input() name: string | undefined;
   level = Math.round(Math.random() * 100);
 
-  constructor() {}
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
