fdf25b9baf97dfb91013ca910e4a6057d93c03c3
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-22 16:57:56 +0200

    Utilisation d'un map avec l'observable

--- src/app/pages/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { delay, lastValueFrom, map, Observable } from 'rxjs';
 import { PokemonsResult, SimplePokemon } from 'src/app/models/api-results/pokemons';
 
 @Component({
@@ -19,14 +19,25 @@ export class PokedexComponent implements OnInit {
   }
 
   initPokemons() {
-    this.getPokemons().subscribe((res: PokemonsResult) => {
+/*     this.getPokemons().subscribe((res: PokemonsResult) => {
       this.pokemons = res.results;
+    }); */
+
+    this.getOnlyPokemons().subscribe((pokemons: SimplePokemon[]) => {
+      this.pokemons = pokemons;
     });
   }
 
   async initPokemonsPromise() {
     const pokemonsResult: PokemonsResult = await this.getPokemonsPromise();
     this.pokemons = pokemonsResult.results;
+  }
+
+  getOnlyPokemons(): Observable<SimplePokemon[]> {
+    return this.getPokemons().pipe(
+      delay(1000),
+      map((pokemonsResult: PokemonsResult) => pokemonsResult.results),
+    );
   }
 
   getPokemons() {
