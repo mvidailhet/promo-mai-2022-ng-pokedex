d659c7ffa57b7d36290ac21e94f98f110d0a6e85
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-23 14:51:36 +0200

    Démo du Promise.all

--- src/app/pages/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex.component.ts
@@ -22,13 +22,41 @@ export class PokedexComponent implements OnInit {
   }
 
   initPokemons() {
-    this.apiService.getPokemons().subscribe(async(pokemonsResult: PokemonsResult) => {
-      this.pokemons = pokemonsResult.results;
-      this.pokemons.forEach(async (pokemon: SimplePokemon, index) => {
-        const pokemonResponse: PokemonResult = await this.apiService.getPokemonfromUrlPromise(pokemon.url);
-        this.pokemons[index] = { ...this.pokemons[index], details: pokemonResponse };
-      })
-    });
+    this.apiService
+      .getPokemons()
+      .subscribe(async (pokemonsResult: PokemonsResult) => {
+        this.pokemons = pokemonsResult.results;
+
+        const pokemonDetailsPromises = this.pokemons.map(
+          (pokemon: SimplePokemon) => {
+            return this.apiService.getPokemonfromUrlPromise(pokemon.url);
+          }
+        );
+
+        // On attend que toutes les Promises soient retournées
+        // Puis on ajoute les images (pas du tout ce qu'on veut pour cet exemple)
+        Promise.all(pokemonDetailsPromises).then(
+          (pokemonResults: PokemonResult[]) => {
+            pokemonResults.forEach(
+              (pokemonResult: PokemonResult, index: number) => {
+                this.pokemons[index] = {
+                  ...this.pokemons[index],
+                  details: pokemonResult,
+                };
+              }
+            );
+          }
+        );
+
+/*         this.pokemons.forEach(async (pokemon: SimplePokemon, index) => {
+          const pokemonResponse: PokemonResult =
+            await this.apiService.getPokemonfromUrlPromise(pokemon.url);
+          this.pokemons[index] = {
+            ...this.pokemons[index],
+            details: pokemonResponse,
+          };
+        }); */
+      });
   }
 
   async initPokemonsPromise() {
